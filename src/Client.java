22fdbab1ccec48046a1c6a54f0e7c740b978df8f
lllena <51027280+lllena@users.noreply.github.com>
2020-05-04 22:51:52 +0300

    Add files via upload

---
+++ src/Client.java
@@ -0,0 +1,39 @@
+public class Client {
+
+    private String name;
+
+    private String login;
+
+    private String password;
+
+    private int discount;
+
+    private String address;
+
+    private int phone;
+
+    private int idClient;
+
+    public Client(int name, int login, int password) {
+    }
+
+    public String getInfo() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public int orderCreation(int idClient, int discount) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public int orderCreation(int idClient) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public int getIdClient() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public int setDiscount(int discount) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
---
+++ src/Employee.java
@@ -0,0 +1,20 @@
+public class Employee {
+
+    private String name;
+
+    private String position;
+
+    private int phone;
+
+    private String address;
+
+    public Employee(String name, String position, int phone, String address) {
+    }
+
+    public String getInfo() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void orderFulfillment() {
+    }
+}
